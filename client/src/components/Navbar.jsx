293b9e9083255735659f44d736267b770eb9c505
AayushGajbhiye9860 <annanyaukey87@gmail.com>
2026-03-18 20:10:04 +0530

    update project files

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0 flex items-center">
-            <Link to="/" className="font-poppins font-bold text-xl text-black">
+            <Link to="/" className="font-heading font-bold text-2xl tracking-tighter text-black uppercase">
               Driplens
             </Link>
           </div>
